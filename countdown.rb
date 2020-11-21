c91d2245b70e09259324ffb1dfb3d7a3f925f5c2
Clint Richards <richardsclint17@gmail.com>
2020-11-21 06:17:25 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,3 @@
 #write your code here
 
-def countdown
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
 
-  seconds_to_midnight.to_s + " left. HAPPY NEW YEAR!"
-end
-end
-end
